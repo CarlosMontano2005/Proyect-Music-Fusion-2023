3de700e09af4da6f676876a4c1b2b13a0949ac20
Monico <gerarmonico511e@gmail.com>
2023-02-17 09:22:50 -0600

    alert de confirmacion de eliminacion creada

---
+++ js/script_alert_delete.js
@@ -0,0 +1,18 @@
+  function mostrar(){
+
+    swal({
+        title: "Estas seguro de que deseas eliminar los registros?",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      })
+      .then((willDelete) => {
+        if (willDelete) {
+          swal("Los registros se han eliminado exitosamente", {
+            icon: "success",
+          });
+        } else {
+          swal("Acción cancelada");
+        }
+      });
+  }
